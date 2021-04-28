433ab7d1ed9fb7e736366b3f3df08d85d763ba97
MohammedAhmedArif <58371318+MohammedAhmedArif@users.noreply.github.com>
2021-04-28 03:22:56 +0200

    Create b18.py

---
+++ b18.py
@@ -0,0 +1,6 @@
+copy = input("Enter text\n>>")
+entry = copy
+print("Length of text:",len(entry))
+print("Text in uppercase:",entry.upper())
+print("Replacing all e's with 3:",entry.replace("e","3"))
+print("Making a list of the text:",entry.split())
