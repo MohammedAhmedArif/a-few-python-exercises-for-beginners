b373335f793e53c9bb1bf4742b791490bb5b73fb
MohammedAhmedArif <58371318+MohammedAhmedArif@users.noreply.github.com>
2021-04-28 03:24:38 +0200

    Delete 5.py

--- 5.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("enter first number-> "))
-num2 = float(input("enter second number-> "))
-print(num1,"+",num2,"=",num1+num2)
-print(num1,"-",num2,"=",num1-num2)
-print(num1,"*",num2,"=",num1*num2)
-print(num1,"/",num2,"=",num1/num2)
