7b3609c899bbc1b154e4ffda6b5d64c2df21c643
MohammedAhmedArif <58371318+MohammedAhmedArif@users.noreply.github.com>
2021-04-28 03:24:17 +0200

    Delete 11.py

--- 11.py
+++
@@ -1,8 +0,0 @@
-total=0
-entry=""
-while(entry != "exit"):
-    entry=input("Enter a number to add it to the total value,enter exit to terminate program\n>>").lower()
-    if entry.isdigit():
-        total+=int(entry)
-    print("total is:",total)
-    
